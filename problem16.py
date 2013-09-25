442aff649bb6ac2187e222d75837c7a8d381793f
root <root@yy923.(none)>
2013-09-25 11:11:23 +0800

    use sum function

--- problem16.py
+++ problem16.py
@@ -1,21 +1,15 @@
 def multiples(n):
     a = n / 15
     b = n % 15
-    print a, b
     return a, b
 
 
-def sum(n):
+def sum1(n):
     a, b = multiples(n)
     result = (32768 ** a) * (2 ** b)
     return result
 
 if __name__ == "__main__":
-    a = str(sum(1000))
-    print a
-    print len(a)
-    number = 0
-    for i in a:
-        number += int(i)
-
-    print number
+    print sum([int(i) for i in str(sum1(1000))])
+    print sum(map(lambda x: int(x), str(2 ** 1000)))
+    print sum([int(i) for i in str(2 ** 1000)])
